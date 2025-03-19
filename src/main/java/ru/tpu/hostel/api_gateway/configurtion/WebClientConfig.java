bddd650861a4e80ef77dd5631dd81d8158907e91
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2025-03-19 19:04:50 +0700

    Перекрестились и наблюдаем пройдет магия или нет

--- src/main/java/ru/tpu/hostel/api_gateway/configurtion/WebClientConfig.java
+++ src/main/java/ru/tpu/hostel/api_gateway/configurtion/WebClientConfig.java
@@ -1,6 +1,5 @@
 package ru.tpu.hostel.api_gateway.configurtion;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
